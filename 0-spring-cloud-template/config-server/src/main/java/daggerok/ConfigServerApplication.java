80032a9ce2791bfd913b7e0487a9ca974388f4f4
Maksim Kostromin <daggerok@gmail.com>
2017-09-24 05:48:46 +0300

    add config folder

--- 0-spring-cloud-template/config-server/src/main/java/daggerok/ConfigServerApplication.java
+++ 0-spring-cloud-template/config-server/src/main/java/daggerok/ConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//@EnableConfigServer
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
