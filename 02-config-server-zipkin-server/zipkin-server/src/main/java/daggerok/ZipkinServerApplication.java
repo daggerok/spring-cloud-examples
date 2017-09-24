a4cf33165608ee2946d287b82ef57fb6a3be7c12
Maksim Kostromin <daggerok@gmail.com>
2017-09-24 08:02:14 +0300

    add 02-config-server-zipkin-server

---
+++ 02-config-server-zipkin-server/zipkin-server/src/main/java/daggerok/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package daggerok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZipkinServerApplication.class, args);
+  }
+}
